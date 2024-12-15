5730e4134ca9020b548ad93c5ac370262a27472f
Raha Heidari <heidariraha761@gmail.com>
2024-12-15 13:18:44 -0800

    js3

---
+++ 3-HarryPotter.js
@@ -0,0 +1,27 @@
+//! ** You should not modify HTML codes.
+//? The photos are duplicated and wrong. :((
+// todo1: please go and find a picture for each character that mentioned in cards in HarryPotter.html .
+// todo2: assign the picutre of each character to it's card.
+
+/**WRITE CODE BELOW */
+
+const imgHermoin = document.querySelector("#Hermione-Granger")
+imgHermoin.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0Tv9m-2pBhE-YbZUmiRRAr42L0unrl3pKbg&s"
+
+const imgWeasley = document.querySelector("#Ron-Weasley")
+imgWeasley.src = "https://media.licdn.com/dms/image/v2/C4E03AQG-bs1KXVf4hQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1655988827203?e=2147483647&v=beta&t=6fKNGEwunXBHx6dySZVnbguo84HboFi8ifQQkc5Tbnc"
+
+const imgSnape = document.querySelector("#Severus-Snape")
+imgSnape.src = "https://metro.co.uk/wp-content/uploads/2015/03/severus-snape-severus-snape-14207617-800-600-e1443881675452.jpg?quality=90&strip=all&w=646"
+
+const imgMalfoy = document.querySelector("#Draco-Malfoy")
+imgMalfoy.src = "https://miro.medium.com/v2/resize:fit:1400/1*3u1GM9w8XN2A704Dd3DbDQ@2x.jpeg"
+
+const imgDiggory = document.querySelector("#Cedric-Diggory")
+imgDiggory.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBpVsVvI2oxG2-9RygT5nlJmethJvP05Bh-Hb5-KTbCODiKS_no0eEF1mD0I4Nzu-h6XQ&usqp=CAU"
+
+const imgVoldemort = document.querySelector("#Lord-Voldemort")
+imgVoldemort.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrRxll9v5DlEKkAtlcXsioeum9dV-HlorIJw&s"
+
+const imgLestrange = document.querySelector("#Bellatrix-Lestrange")
+imgLestrange.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGfxFdNSsHlFk6QOQ2uW08qun57mqpoCmN7Mx8Munn5YFtoaN8I0zEt6hwuo-roIOFWRM&usqp=CAU"
